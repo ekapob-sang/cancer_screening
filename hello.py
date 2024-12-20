efed652b82085f3baeee352b6e2dfe3d4b162faf
ekapob-sang <76510467+ekapob-sang@users.noreply.github.com>
2024-12-20 22:00:25 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,13 +7,14 @@ st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
 #st.image(image2,width=600)
 st.subheader('''📖 This page provides Thailand's cancer screening program data in budget year 2025''')
 
-if st.button("Page 1"):
-    st.switch_page("screening_mammogram.py")
-  
 st.subheader('👈xx Please select the graphs from this sidebar.' )
 st.subheader('📊 1. Mammogram screening for breast cancer in high risk group')
+if st.button("Mammogram"):
+    st.switch_page("pages\screening_mammogram.py")
 st.markdown('Update data every Monday')
 st.subheader('📊 2. HPV test for cervical cancer')
+if st.button("HPV"):
+    st.switch_page("pages\2_screening_hpv.py")
 st.markdown('Update data monthly')
 st.text('')
 st.markdown('ที่มุมขวาล่าง ท่านสามารถขยายหน้าจอ,save dashboard หรือส่งต่อลิงค์')
