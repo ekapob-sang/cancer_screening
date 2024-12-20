9dfda9bb2b7fd05f94da57e895a117e115326ec7
ekapob-sang <76510467+ekapob-sang@users.noreply.github.com>
2024-12-20 21:46:25 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,15 +7,21 @@ st.set_page_config(layout="wide",initial_sidebar_state="collapsed")
 #st.image(image2,width=600)
 st.subheader('''📖 This page provides Thailand's cancer screening program data in budget year 2025''')
 
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
+if 'clicked1' not in st.session_state:
+    st.session_state.clicked1 = False
+if 'clicked2' not in st.session_state:
+    st.session_state.clicked2 = False
 
-def click_button():
-    st.session_state.clicked = True
+def click_button1():
+    st.session_state.clicked1 = True
+    st.session_state.clicked2 = False
+def click_button2():
+    st.session_state.clicked2 = True
+    st.session_state.clicked1 = False
 
-st.button('Click me', on_click=click_button)
+st.button('Click me', on_click=click_button1)
 
-if st.session_state.clicked:
+if st.session_state.clicked1:
     with st.container():
      st.write("This is inside the container")
     # You can call any Streamlit command, including custom components:
@@ -24,8 +30,8 @@ if st.session_state.clicked:
      <div class='tableauPlaceholder' id='viz1734327680763' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;nh&#47;nhso_mammo&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='nhso_mammo&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;nh&#47;nhso_mammo&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='th-TH' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1734327680763');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';} else { vizElement.style.width='100%';vizElement.style.height='1827px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
     """, height=1000 , width =1200  )
 
-st.button('Click me2', on_click=click_button)
-if st.session_state.clicked:
+st.button('Click me2', on_click=click_button2)
+if st.session_state.clicked2:
     with st.container():
      st.write("This is inside the container")
     # You can call any Streamlit command, including custom components:
